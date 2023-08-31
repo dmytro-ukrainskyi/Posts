c6509535f3a9fadb16a9d8b90bbd48e0acc32d24
Dmytro Ukrainskyi <dmytro.ukrainskyi@gmail.com>
2023-08-31 18:55:06 +0300

    Initial Commit

---
+++ Posts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts
+//
+//  Created by Dmytro Ukrainskyi on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
